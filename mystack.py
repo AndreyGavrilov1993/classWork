06133be609762df6d5f62d2c487850d7d1702f30
AndreyGavrilov1993 <127882158+AndreyGavrilov1993@users.noreply.github.com>
2023-03-30 21:11:24 +0300

    Add files via upload

--- mystack.py
+++ mystack.py
@@ -8,53 +8,102 @@
 # print(st.getName())
 
 
-import random
-class Stack:
+# import random
+# class Stack:
+#
+#     def __init__(self,size):
+#      self.__size=size
+#      self.__counter=0
+#      self.__list=['' for i in range(size)]
+#
+#     def size(self):
+#         return self.__counter
+#
+#     def show(self):
+#         # print("[",end=" ")
+#         # for i in range(self.__counter-1):
+#         #     print(self.__list[i],end=", ")
+#         # print(self.__list[self.__counter],"]")
+#         print(self.__list[:self.__counter:])
+#
+#     def push(self, n):
+#         if self.__counter==self.__size:
+#             raise IndexError('list index out of range')
+#         else:
+#             self.__list[self.__counter] = n
+#             self.__counter+=1
+#     def pop(self):
+#          if self.__counter==0:
+#              raise IndexError('list is empty')
+#          else:
+#              self.__counter-=1
+#
+#
+#
+# if __name__=="__main__":
+#  try:
+#   stack1 = Stack(4)
+#   stack1.show()
+#   print(stack1.size())
+#   for i in range(7):
+#       stack1.push(random.randint(1,9))
+#   stack1.show()
+#   print(stack1.size())
+#   for i in range(2):
+#       stack1.pop()
+#   stack1.show()
+#   print(stack1.size())
+#   for i in range(2):
+#       stack1.pop()
+#   stack1.show()
+#  except Exception as er:
+#     print(er)
 
+import random
+class Queue:
     def __init__(self,size):
-     self.__size=size
-     self.__counter=0
-     self.__list=['' for i in range(size)]
+        self.__size=size
+        self.__counter=0
+        self.__list=['' for i in range(9)]
 
     def size(self):
         return self.__counter
-
     def show(self):
         # print("[",end=" ")
         # for i in range(self.__counter-1):
         #     print(self.__list[i],end=", ")
         # print(self.__list[self.__counter],"]")
         print(self.__list[:self.__counter:])
-
     def push(self, n):
         if self.__counter==self.__size:
-            raise IndexError('list index out of range')
+            raise IndexError('List index out of range')
         else:
             self.__list[self.__counter] = n
             self.__counter+=1
-    def pop(self):
-         if self.__counter==0:
-             raise IndexError('list is empty')
-         else:
-             self.__counter-=1
-
-
+    def __pop__(self):
+        if self.__counter==0:
+            raise IndexError('List is empty')
+        else:
+            for i in range(self.__counter-1):
+                self.__list[i]=self.__list[i+1]
+            self.__counter-=1
 
 if __name__=="__main__":
- try:
-  stack1 = Stack(4)
-  stack1.show()
-  print(stack1.size())
-  for i in range(7):
-      stack1.push(random.randint(1,9))
-  stack1.show()
-  print(stack1.size())
-  for i in range(2):
-      stack1.pop()
-  stack1.show()
-  print(stack1.size())
-  for i in range(2):
-      stack1.pop()
-  stack1.show()
- except Exception as er:
-    print(er)
+    try:
+        queue1 = Queue(9)
+        queue1.show()
+        print(queue1.size())
+        for i in range(7):
+            queue1.push(random.randint(1,9))
+        queue1.show()
+        print(queue1.size())
+        for i in range(2):
+            queue1.__pop__()
+        queue1.show()
+        print(queue1.size())
+        for i in range(2):
+            queue1.__pop__()
+        queue1.show()
+    except Exception as er:
+        print(er)
+
